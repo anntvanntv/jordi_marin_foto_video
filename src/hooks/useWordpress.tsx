f25586eaf8aad063f8154e4bd4faba7203597e5e
Anna Taules <anna.taules@gmail.com>
2026-04-07 19:02:15 +0200

    api nueva

--- src/hooks/useWordpress.tsx
+++ src/hooks/useWordpress.tsx
@@ -1,23 +1,24 @@
 import { useState, useEffect } from "react";
 
 type WPPost = {
-    id: number;
-    title: { rendered: string };
-    _embedded?: {
-      "wp:featuredmedia"?: { source_url: string }[];
-    };
-  }; 
+  id: number;
+  title: { rendered: string };
+  _embedded?: {
+    "wp:featuredmedia"?: { source_url: string }[];
+  };
+};
 
 export const useWordpress = (tipo: string) => {
-    const [items, setItems] = useState<WPPost[]>([]);
+  const [items, setItems] = useState<WPPost[]>([]);
 
+  useEffect(() => {
+    const API = import.meta.env.VITE_WORDPRESS_API;
 
-    useEffect(() => {
-        fetch(`https://jmp-studiobcn.com/wp-json/wp/v2/${tipo}?_embed`)
-             .then(res => res.json())
-             .then(data => setItems(data))
-             .catch(err => console.error(err));
-    }, [tipo]);
+    fetch(`${API}/${tipo}?_embed`)
+      .then(res => res.json())
+      .then(data => setItems(data))
+      .catch(err => console.error(err));
+  }, [tipo]);
 
-    return items;
+  return items;
 };
